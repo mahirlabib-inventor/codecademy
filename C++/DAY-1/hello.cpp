a69711c4a6e67dce570b1d673c038714f2fedf2d
mahirlabib-inventor <60250437+mahirlabib-inventor@users.noreply.github.com>
2020-07-23 22:43:16 +0600

    v5

--- C++/DAY-1/hello.cpp
+++ C++/DAY-1/hello.cpp
@@ -3,9 +3,7 @@
 int main() 
 {
   
-  std::cout << "       1\n";
-  std::cout << "     2 3\n";
-  std::cout << "   4 5 6\n";
-  std::cout << "7 8 9 10\n";
+	std::cout << "Hello\n";
+	std::cout << "Goodbye\n";
     
 }
