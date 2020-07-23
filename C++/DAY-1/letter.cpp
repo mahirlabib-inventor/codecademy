07ae5b34cf079fe124f3edf064ad1cad718fa025
mahirlabib-inventor <60250437+mahirlabib-inventor@users.noreply.github.com>
2020-07-23 22:46:43 +0600

    v1

---
+++ C++/DAY-1/letter.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "Dear Self,\n";
+  std::cout << "I want to be MIT math professor\n";
+  std::cout << "Mahir Labib and 23July2020\n";
+  
+  
+  
+  
+  
+}
