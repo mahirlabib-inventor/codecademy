2d2d9538018ed3f0c40dd33ddfa0f05a47b7f256
mahirlabib-inventor <60250437+mahirlabib-inventor@users.noreply.github.com>
2020-07-24 09:01:14 +0600

    Update variable.cpp

--- C++/DAY-3/variable.cpp
+++ C++/DAY-3/variable.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
 int main() {
-  
-  // Declare a variable   
-  int year;
-  year = 2019;
-  
-  // Initialize a variable
-  
+
+   int year = 2019;
+   
+   // Declare and initialize a variable here
+   
+   
+   return 0;
    
 }
