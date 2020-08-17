451ccc96372647a54bcc78fd888e806dad9992f3
mahirlabib-inventor <60250437+mahirlabib-inventor@users.noreply.github.com>
2020-08-17 16:07:13 +0600

    created

---
+++ C++/DAY-3/temperature.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+  double tempf, tempc;
+
+  std::cin>> tempf;
+
+  tempc = (tempf-32)/1.8;
+
+  
+  std::cout << "The temp is "<<tempc<<" degrees Celsius.\n";
+  
+  
+  
+  
+}
